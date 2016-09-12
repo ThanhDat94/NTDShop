46aefef1043b728c0416e7c1eced1874adbe67fa
Thanhdat <nguyenthanhdat020494@gmail.com>
2016-09-12 17:44:44 +0700

    data accesslayer 2

---
+++ NTDShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace NTDShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private NTDShopDbContext dbContext;
+
+        public NTDShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new NTDShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
---
+++ NTDShop.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTDShop.Data.Infrastructure
+{
+    public class UnitOfWork:IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private NTDShopDbContext dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public NTDShopDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
