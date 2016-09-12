cd82627e238531f7d190860c3efb9c0381b8ab6f
Thanhdat <nguyenthanhdat020494@gmail.com>
2016-09-12 16:15:35 +0700

    add model entity

---
+++ NTDShop.Model/Models/Order.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTDShop.Model.Models
+{
+    [Table("Orders")]
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string CustomerName { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string CustomerAddress { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Mobile { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string CustomerEmail { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string CustomerMessage { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public string CreateBy { get; set; }
+        [Required]
+        [MaxLength(256)]
+        public string PaymentMethod { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string PaymentStatus { get; set; }
+        public bool Status { get; set; }
+        public virtual IEnumerable<OrderDetail> OrderDetail { get; set; }
+    }
+}
