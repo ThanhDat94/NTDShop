9c805fc63f91ae8521aca8adc243b6039d286f4b
Thanhdat <nguyenthanhdat020494@gmail.com>
2016-09-17 21:51:59 +0700

    this is test PostCategoryrepositoryTest

---
+++ NTDShop.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NTDShop.Data.Infrastructure;
+using NTDShop.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTDShop.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        IDbFactory dbFactory;
+        IPostCategoryRepository objRepository;
+        IUnitOfWork unitOfWork;
+        [TestInitialize]
+        public void Initalize()
+        {
+            dbFactory = new DbFactory();
+            objRepository = new PostCategoryRepository(dbFactory);
+            unitOfWork = new UnitOfWork(dbFactory);
+        }
+
+        public void PostCategory_Repository_GetAll()
+        {
+            var list = objRepository.GetAll().ToList();
+
+        }
+    }
+}
