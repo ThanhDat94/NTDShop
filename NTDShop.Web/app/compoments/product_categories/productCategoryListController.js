7332bf93098a3884bedeb8c59edad76ce78487bd
Thanhdat <nguyenthanhdat020494@gmail.com>
2016-09-29 13:55:57 +0700

    notification

--- NTDShop.Web/app/compoments/product_categories/productCategoryListController.js
+++ NTDShop.Web/app/compoments/product_categories/productCategoryListController.js
@@ -2,9 +2,9 @@
 (function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject = ['$scope', 'apiService'];
+    productCategoryListController.$inject = ['$scope', 'apiService', 'notificationService'];
 
-    function productCategoryListController($scope, apiService) {
+    function productCategoryListController($scope, apiService, notificationService) {
 
         $scope.productCategories = [];
         $scope.page = 0;
@@ -27,6 +27,12 @@
                 }
             }
             apiService.get('/api/productCategory/getall', config, function (result) {
+                if (result.data.TotalCount == 0) {
+                    notificationService.displayWarning('Không tìm thấy kết quả');
+                }
+                else {
+                    notificationService.displaySuccess('Đã tìm thấy ' + result.data.TotalCount +  ' Bản Ghi');
+                }
                 $scope.productCategories = result.data.Items;
                 $scope.page = result.data.Page;
                 $scope.pagesCount = result.data.TotalPages;
